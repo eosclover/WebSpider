c74b1911b79cc12911f1b43c9048adc185b02b08
eosclover <ft_clover@163.com>
2018-09-14 22:41:12 +0800

    补充第三方包的安装方式，共三种

--- Common/csvfile_write.py
+++ Common/csvfile_write.py
@@ -42,19 +42,34 @@
 #
 
 #
-# #读取CSV文件的内容
-# import csv
-# with open('data.csv','r',encoding='utf-8') as csvfile:
-#     reader=csv.reader(csvfile)
-#     for row in reader:
-#         print(row)
+#读取CSV文件的内容
+import csv
+with open('data.csv','r',encoding='utf-8') as csvfile:
+    reader=csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+#第三方包安装
+"""
+1.python：用setup.py安装第三方包packages
+
+（1）先下载要安装的包，并下载到磁盘下
+
+（2）进入到该文件的setup.py目录下，打开cmd,并切换到该目录
+
+（3）先执行python setup.py build
+
+（4）然后在执行python  setup.py install
+2.已经安装了pip,并将pip的路径放到系统变量的path路径下，直接执行，pip install pandas=0.23.0
+3.也可在pycharm中安装第三方插件
+打开pycharm工具，file--settings--plugins,搜索要安装的插件，点击安装即可
+"""
 
 
 #利用pandas读取csv文件
 import pandas as pd
 df=pd.read_csv('data.csv')
 print(df)
-
 
 
 
